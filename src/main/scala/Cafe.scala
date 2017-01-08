aaf25c0d2b639cd0c1761746fafbb237d49f0425
Camille Ballouche <Camille.Ballouche@Ticketmaster.co.uk>
2017-01-08 22:46:34 +0000

    - Step 2 : Add service charges - Implement rules for the service charges based on the types of items purchased.

--- src/main/scala/Cafe.scala
+++ src/main/scala/Cafe.scala
@@ -1,14 +1,52 @@
 sealed trait Item {
   val price: Double
 }
+sealed trait Drink extends Item
+sealed trait Food extends Item
+sealed trait Hot
 
-case class Cola(price: Double = 0.5)
-case class Coffee(price: Double = 1) extends Item
-case class CheeseSandwich(price: Double = 2) extends Item
-case class SteakSandwich(price: Double = 4.5) extends Item
+case class Cola(price: Double = 0.5) extends Drink
+case class Coffee(price: Double = 1) extends Drink with Hot
+case class CheeseSandwich(price: Double = 2) extends Food
+case class SteakSandwich(price: Double = 4.5) extends Food with Hot
+
 
 object Cafe {
 
+  val MAX_SERVICE_CHARGE = 20
+  val FOOD_SERVICE_CHARGE = 1.1
+  val HOT_FOOD_SERVICE_CHARGE = 1.2
+
   def bill(items: Seq[Item]) = items.foldLeft(0.0)(_ + _.price)
 
+  def billWithCharges(items: Seq[Item]) = {
+    val onlyDrinks = items.forall {
+      case drink: Drink => true
+      case _ => false
+    }
+    val hasHotFood = items exists {
+      case drink: Food with Hot => true
+      case _ => false
+    }
+    val hasFood = items exists {
+      case drink: Food => true
+      case _ => false
+    }
+
+    val totalBill = bill(items)
+
+    (onlyDrinks, hasHotFood, hasFood) match {
+      case (true, _, _) => totalBill
+      case (_, true, _) =>
+        if (totalBill * 0.2 < 20)
+          roundBill(totalBill * HOT_FOOD_SERVICE_CHARGE)
+        else
+          roundBill(totalBill + MAX_SERVICE_CHARGE)
+      case (_, _, true) => roundBill(totalBill * FOOD_SERVICE_CHARGE)
+    }
+  }
+
+  def roundBill(total: Double) = {
+    BigDecimal(total).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
+  }
 }
--- src/test/scala/CafeTest.scala
+++ src/test/scala/CafeTest.scala
@@ -10,4 +10,24 @@ class CafeTest extends FlatSpec with Matchers {
   it should "return 0 for an empty list of items" in {
     bill(List.empty) should be (0)
   }
+
+  "BillWithCharges" should "add any service charge if only drinks were purchased" in {
+    billWithCharges(List(Cola(), Coffee())) should be (1.5)
+  }
+
+  it should "add 10% to the bill if any food was purchased" in {
+    billWithCharges(List(Cola(), Coffee(), CheeseSandwich())) should be (3.85)
+  }
+
+  it should "add 20% to the bill if any hot food was purchased" in {
+    billWithCharges(List(Cola(), Coffee(), SteakSandwich())) should be (7.2)
+  }
+
+  it should "limit the service charges to 20 if any hot food was purchased" in {
+    billWithCharges(List(SteakSandwich(110))) should be (130)
+  }
+
+  "roundBill" should "round the bill to 2 decimals" in {
+    roundBill(1.2222) should be (1.22)
+  }
 }
